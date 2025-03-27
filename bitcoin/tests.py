859d9241edbcb75f72aa741d9a49e027b5dfd622
Tomas <expositotomas2002@gmail.com>
2025-03-27 14:58:16 +0000

    refactor(btc): unit test refactor

--- bitcoin/tests.py
+++ bitcoin/tests.py
@@ -30,7 +30,7 @@ class PoWBlockTestCase(unittest.TestCase):
             parent="This is a test!",
             target="This is still a test!",
         )
-
+    
     def test01_merkle_root(self):
         """Test Case 01: Merkle Roots"""
         # Test that the method works as intended
@@ -107,282 +107,108 @@ class PoWNodeTest(unittest.TestCase):
             target="This is still a test!",
         )
         cls.txid = list(cls.block.transactions.keys())[0]
-        cls.node = PoWNode(pub=cls.pub, priv=cls.priv)
 
+        cls.node = PoWNode(pub=cls.pub, priv=cls.priv)
         cls.node.blockchain = [cls.block]
         cls.node.utxo_set = {cls.txid: UTXO(v_outs=[0, 1], block_id=0)}
         cls.node.transactions = []
 
-    def test01_coin_transfer(self):
-        """Test 1: Transaction with valid input owner and acceptable amount"""
-        fee = self.node.validate_transaction(
-            {
+        cls.coin_transaction = {
                 "version": 1,
                 "inputs": [
                     {
-                        "tx_id": self.txid,
+                        "tx_id": cls.txid,
                         "v_out": 0,
-                        "key": dump_pubkey(self.pub),
-                        "signature": sign(self.priv, str(10000)),
+                        "key": dump_pubkey(cls.pub),
+                        "signature": sign(cls.priv, str(10000)),
                     }
                 ],
                 "outputs": [
                     {
                         "amount": 1000,
-                        "keyhash": hash_pubkey(self.otherpub),
+                        "keyhash": hash_pubkey(cls.otherpub),
                     },
                     {
                         "amount": 8999,
-                        "keyhash": hash_pubkey(self.pub),
+                        "keyhash": hash_pubkey(cls.pub),
                     },
                 ],
             }
-        )
-        self.assertNotEqual(fee, False)
-        self.assertEqual(fee, 1)
 
-    def test02_data_transfer(self):
-        """Test 7: Transaction where input data is permanently lost"""
-        self.assertFalse(
-            self.node.validate_transaction(
-                {
+        cls.data_transaction = {
                     "version": 1,
                     "inputs": [
                         {
-                            "tx_id": self.txid,
+                            "tx_id": cls.txid,
                             "v_out": 1,
-                            "key": dump_pubkey(self.pub),
-                            "signature": sign(self.priv, "some-bytes"),
+                            "key": dump_pubkey(cls.pub),
+                            "signature": sign(cls.priv, "some-bytes"),
                         },
                     ],
                     "outputs": [
                         {
                             "data": "some-bytes",
-                            "keyhash": hash_pubkey(self.otherpub),
+                            "keyhash": hash_pubkey(cls.otherpub),
                         },
                         {
                             "data": "some-more-bytes",
-                            "keyhash": hash_pubkey(self.pub),
+                            "keyhash": hash_pubkey(cls.pub),
                         },
                     ],
                 }
-            )
-        )
+
+    def test01_coin_transfer(self):
+        """Test 1: Transaction with valid input owner and acceptable amount"""
+        fee = self.node.validate_transaction(self.coin_transaction)
+        self.assertNotEqual(fee, False)
+        self.assertEqual(fee, 1)
+
+    def test02_data_transfer(self):
+        """Test 2: Transaction where input data is transfered correctly"""
+        fee = self.node.validate_transaction(self.data_transaction)
+        self.assertIsNot(fee, False)
+        self.assertEqual(fee, 0)
+
 
     def test03_invalid_outpoint(self):
-        """Test 2: Transaction with invalid outpoint field"""
-        self.assertFalse(
-            self.node.validate_transaction(
-                {
-                    "version": 1,
-                    "inputs": [
-                        {
-                            "tx_id": "potato",
-                            "v_out": 0,
-                            "key": dump_pubkey(self.pub),
-                            "signature": sign(self.priv, str(10000)),
-                        },
-                    ],
-                    "outputs": [
-                        {
-                            "amount": 1000,
-                            "keyhash": hash_pubkey(self.pub),
-                        },
-                        {
-                            "amount": 8999,
-                            "keyhash": hash_pubkey(self.pub),
-                        },
-                    ],
-                }
-            )
-        )
-        self.assertFalse(
-            self.node.validate_transaction(
-                {
-                    "version": 1,
-                    "inputs": [
-                        {
-                            "tx_id": self.txid,
-                            "v_out": "invalid",
-                            "key": dump_pubkey(self.pub),
-                            "signature": sign(self.priv, str(10000)),
-                        },
-                    ],
-                    "outputs": [
-                        {
-                            "amount": 1000,
-                            "keyhash": hash_pubkey(self.pub),
-                        },
-                        {
-                            "amount": 8999,
-                            "keyhash": hash_pubkey(self.pub),
-                        },
-                    ],
-                }
-            )
-        )
+        """Test 3: Transaction with invalid outpoint field"""
+        txid = self.coin_transaction["inputs"][0]["tx_id"]
+        
+        self.coin_transaction["inputs"][0]["tx_id"] = "potato"
+        self.assertFalse(self.node.validate_transaction(self.coin_transaction))
+
+        self.coin_transaction["inputs"][0]["tx_id"] = txid
+        self.coin_transaction["inputs"][0]["v_out"] = "invalid"
+        self.assertFalse(self.node.validate_transaction(self.coin_transaction))
 
     def test04_double_spending(self):
-        """Test 3: Transaction with repeated input field"""
-        self.assertFalse(
-            self.node.validate_transaction(
-                {
-                    "version": 1,
-                    "inputs": [
-                        {
-                            "tx_id": self.txid,
-                            "v_out": 0,
-                            "key": dump_pubkey(self.pub),
-                            "signature": sign(self.priv, str(10000)),
-                        },
-                        {
-                            "tx_id": self.txid,
-                            "v_out": 0,
-                            "key": dump_pubkey(self.pub),
-                            "signature": sign(self.priv, str(10000)),
-                        },
-                    ],
-                    "outputs": [
-                        {
-                            "amount": 1000,
-                            "keyhash": hash_pubkey(self.pub),
-                        },
-                        {
-                            "amount": 8999,
-                            "keyhash": hash_pubkey(self.pub),
-                        },
-                    ],
-                }
-            )
-        )
+        """Test 4: Transaction with repeated input field"""
+        self.coin_transaction["inputs"].append(self.coin_transaction["inputs"][0])
+        self.assertFalse(self.node.validate_transaction(self.coin_transaction))
+        
 
     def test05_invalid_pubkey(self):
-        """Test 4: Transaction with invalid public key hash"""
-        self.assertFalse(
-            self.node.validate_transaction(
-                {
-                    "version": 1,
-                    "inputs": [
-                        {
-                            "tx_id": self.txid,
-                            "v_out": 0,
-                            "key": dump_pubkey(self.otherpub),
-                            "signature": sign(self.priv, str(10000)),
-                        },
-                    ],
-                    "outputs": [
-                        {
-                            "amount": 1000,
-                            "keyhash": hash_pubkey(self.pub),
-                        },
-                        {
-                            "amount": 8999,
-                            "keyhash": hash_pubkey(self.pub),
-                        },
-                    ],
-                }
-            )
-        )
+        """Test 5: Transaction with invalid public key hash"""
+        self.coin_transaction["inputs"][0]["key"] = dump_pubkey(self.otherpub)
+        self.assertFalse(self.node.validate_transaction(self.coin_transaction))
 
     def test06_invalid_sign(self):
-        """Test 5: Transaction with valid public key but invalid signature"""
-        self.assertFalse(
-            self.node.validate_transaction(
-                {
-                    "version": 1,
-                    "inputs": [
-                        {
-                            "tx_id": self.txid,
-                            "v_out": 0,
-                            "key": dump_pubkey(self.pub),
-                            "signature": sign(self.otherpriv, str(10000)),
-                        },
-                    ],
-                    "outputs": [
-                        {
-                            "amount": 1000,
-                            "keyhash": hash_pubkey(self.pub),
-                        },
-                        {
-                            "amount": 8999,
-                            "keyhash": hash_pubkey(self.pub),
-                        },
-                    ],
-                }
-            )
-        )
-        self.assertFalse(
-            self.node.validate_transaction(
-                {
-                    "version": 1,
-                    "inputs": [
-                        {
-                            "tx_id": self.txid,
-                            "v_out": 0,
-                            "key": dump_pubkey(self.pub),
-                            "signature": sign(self.priv, str(10001)),
-                        },
-                    ],
-                    "outputs": [
-                        {
-                            "amount": 1000,
-                            "keyhash": hash_pubkey(self.pub),
-                        },
-                        {
-                            "amount": 8999,
-                            "keyhash": hash_pubkey(self.pub),
-                        },
-                    ],
-                }
-            )
-        )
+        """Test 6: Transaction with valid public key but invalid signature"""
+        self.coin_transaction["inputs"][0]["signature"] = sign(self.otherpriv, str(10000))
+        self.assertFalse(self.node.validate_transaction(self.coin_transaction))
+
+        self.coin_transaction["inputs"][0]["signature"] = sign(self.priv, str(10001))
+        self.assertFalse(self.node.validate_transaction(self.coin_transaction))
 
     def test07_invalid_amount(self):
-        """Test 6: Transaction with non-matching input/output amounts"""
-        self.assertFalse(
-            self.node.validate_transaction(
-                {
-                    "version": 1,
-                    "inputs": [
-                        {
-                            "tx_id": self.txid,
-                            "v_out": 0,
-                            "key": dump_pubkey(self.pub),
-                            "signature": sign(self.priv, str(10000)),
-                        },
-                    ],
-                    "outputs": [
-                        {
-                            "amount": 1000,
-                            "keyhash": hash_pubkey(self.pub),
-                        },
-                        {
-                            "amount": 9001,
-                            "keyhash": hash_pubkey(self.pub),
-                        },
-                    ],
-                }
-            )
-        )
+        """Test 7: Transaction with non-matching input/output amounts"""
+        self.coin_transaction["outputs"][1]["amount"] = 9001
+        self.assertFalse(self.node.validate_transaction(self.coin_transaction))
 
     def test08_data_loss(self):
-        """Test 7: Transaction where input data is permanently lost"""
-        self.assertFalse(
-            self.node.validate_transaction(
-                {
-                    "version": 1,
-                    "inputs": [
-                        {
-                            "tx_id": self.txid,
-                            "v_out": 1,
-                            "key": dump_pubkey(self.pub),
-                            "signature": sign(self.priv, "some-bytes"),
-                        },
-                    ],
-                    "outputs": [],
-                }
-            )
-        )
+        """Test 8: Transaction where input data is permanently lost"""
+        self.data_transaction["outputs"] = []
+        self.assertFalse(self.node.validate_transaction(self.data_transaction))
 
 
 if __name__ == "__main__":
